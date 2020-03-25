b728374dc0ee2e6ab0da9c7992a0744d16616097
idriscahyono <idriscahyono@gmail.com>
2020-03-25 21:39:25 +0700

    add jwt produk api

--- src/api/produk/ProdukRouter.js
+++ src/api/produk/ProdukRouter.js
@@ -1,16 +1,17 @@
 const router = require('express').Router()
 const ProdukController = require('./ProdukController')
+const verifyToken = require('../../lib/verifyToken')
 const multer = require('multer')
 const storage = require('../../config/storage')
 const upload = multer({
     storage
 })
 
-router.get('/', ProdukController.index)
-router.get('/:id', ProdukController.show)
-router.post('/', ProdukController.store)
-router.put('/:id', ProdukController.update)
-router.delete('/:id', ProdukController.destroy)
+router.get('/', verifyToken, ProdukController.index)
+router.get('/:id', verifyToken, ProdukController.show)
+router.post('/', verifyToken, ProdukController.store)
+router.put('/:id', verifyToken, ProdukController.update)
+router.delete('/:id', verifyToken, ProdukController.destroy)
 
 router.post('/:id/upload', upload.single("image"), ProdukController.upload)
 
