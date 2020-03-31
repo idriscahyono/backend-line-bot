93e92b99bc274b7cab9cafc865637fb0a85d8333
idriscahyono <idriscahyono@gmail.com>
2020-03-31 20:54:33 +0700

    update

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.get("/cek", verifyToken, function (req, res) {
 
 app.use(router)
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 app.listen(port, function () {
     console.log("Server Running")
 })
