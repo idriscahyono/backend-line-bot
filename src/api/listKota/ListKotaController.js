cd32c582335cf6206c011142cd88ee6015b7df2f
idriscahyono <idriscahyono@gmail.com>
2020-04-29 09:01:40 +0700

    update

--- src/api/listKota/ListKotaController.js
+++ src/api/listKota/ListKotaController.js
@@ -8,7 +8,9 @@ module.exports = {
     },
 
     show: function (req, res) {
-        ListKotaModel.findById(req.params.id).then(function (row) {
+        ListKotaModel.find({
+            city_name: req.body.city_name
+        }).then(function (row) {
             res.send(row)
         })
     },
