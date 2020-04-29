a7b1a20a2cc67e7cb9fb49434e5739573a5c4744
idriscahyono <idriscahyono@gmail.com>
2020-04-29 09:17:53 +0700

    update

--- src/api/listKota/ListKotaRouter.js
+++ src/api/listKota/ListKotaRouter.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const ListKotaController = require('./ListKotaController')
 
 router.get('/', ListKotaController.index)
-router.get('/:id', ListKotaController.show)
+router.get('/kota/', ListKotaController.show)
 router.post('/', ListKotaController.store)
 router.put('/:id', ListKotaController.update)
 router.delete('/:id', ListKotaController.destroy)
