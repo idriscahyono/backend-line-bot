8577ada9d7ba44068207326a94b1b98007ce5417
idriscahyono <idriscahyono@gmail.com>
2020-06-22 09:38:15 +0700

    update

--- src/api/pesanan/PesananController.js
+++ src/api/pesanan/PesananController.js
@@ -1,69 +1,77 @@
-const PesananModel = require('./PesananModel')
+const PesananModel = require('./PesananModel');
 
 module.exports = {
-    index: function (req, res) {
-        PesananModel.find().then(function (row) {
-            res.send(row)
-        })
-    },
+	index: function(req, res) {
+		PesananModel.find().then(function(row) {
+			res.send(row);
+		});
+	},
 
-    showAdmin: function (req, res) {
-        PesananModel.find({
-            nomorBayar: req.params.id,
-            userBayar: true,
-            adminBayar: false
-        }).then(function (row) {
-            res.send(row)
-        })
-    },
+	showAdmin: function(req, res) {
+		PesananModel.find({
+			nomorBayar: req.params.id,
+			userBayar: true,
+			adminBayar: false
+		}).then(function(row) {
+			res.send(row);
+		});
+	},
 
-    showByNomorBayar: function (req, res) {
-        PesananModel.find({
-            nomorBayar: req.params.id,
-        }).then(function (row) {
-            res.send(row)
-        })
-    },
+	showByNomorBayar: function(req, res) {
+		PesananModel.find({
+			nomorBayar: req.params.id
+		}).then(function(row) {
+			res.send(row);
+		});
+	},
 
-    show: function (req, res) {
-        PesananModel.find({
-            userId: req.params.id
-        }).then(function (row) {
-            res.send(row)
-        })
-    },
+	show: function(req, res) {
+		PesananModel.find({
+			userId: req.params.id,
+			userBayar: false,
+			adminBayar: false
+		}).then(function(row) {
+			res.send(row);
+		});
+	},
 
-    store: function (req, res) {
-        PesananModel.create(req.body).then(function (row) {
-            res.send(row)
-        })
-    },
+	store: function(req, res) {
+		PesananModel.create(req.body).then(function(row) {
+			res.send(row);
+		});
+	},
 
-    update: function (req, res) {
-        PesananModel.updateMany({
-                nomorBayar: req.params.id
-            },
-            req.body, {
-                new: true
-            }).then(function (row) {
-            res.send(row)
-        })
-    },
-    updateByUserId: function (req, res) {
-        PesananModel.updateMany({
-                userId: req.params.id
-            },
-            req.body, {
-                new: true
-            }).then(function (row) {
-            res.send(row)
-        })
-    },
-    destroy: function (req, res) {
-        PesananModel.deleteMany({
-            userId: req.params.id
-        }).then(function (row) {
-            res.send(row)
-        })
-    },
-}
+	update: function(req, res) {
+		PesananModel.updateMany(
+			{
+				nomorBayar: req.params.id
+			},
+			req.body,
+			{
+				new: true
+			}
+		).then(function(row) {
+			res.send(row);
+		});
+	},
+	updateByUserId: function(req, res) {
+		PesananModel.updateMany(
+			{
+				userId: req.params.id
+			},
+			req.body,
+			{
+				new: true
+			}
+		).then(function(row) {
+			res.send(row);
+		});
+	},
+	destroy: function(req, res) {
+		PesananModel.deleteMany({
+			userId: req.params.id
+		}).then(function(row) {
+			res.send(row);
+		});
+	}
+};
